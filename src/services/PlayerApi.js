49d490cd44a06afcae7ca128a12a48e5eef42c99
ertyant24 <erikciertugrul@gmail.com>
2023-09-06 23:48:32 +0300

    PlayerApi.js

---
+++ src/services/PlayerApi.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const playerUrl = "/player";
+
+class PlayerApi {
+
+    List() {
+        return axios.get(`${playerUrl}`)
+    }
+
+
+    ListCountOrder(count, order) {
+        return axios.get(`${playerUrl}/${count}/${order}`);
+    }
+
+    
+    ListGetById(id) {
+        return axios.get(`${playerUrl}/${id}`);
+    }
+
+    
+    Create(model) {
+        return axios.post(`${playerUrl}`, model);
+    }
+
+    
+    EditPlayer(id, model) {
+        return axios.put(`${playerUrl}/${id}`, model);
+    }
+
+    
+    Delete(id) {
+        return axios.delete(`${playerUrl}/${id}`);
+    }
+}
+
+export default new PlayerApi();
