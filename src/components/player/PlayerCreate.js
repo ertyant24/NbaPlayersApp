35f940fb8017b6bde68b20873764e18de68ef167
ertyant24 <erikciertugrul@gmail.com>
2023-09-16 16:03:34 +0300

    player add

--- src/components/player/PlayerCreate.js
+++ src/components/player/PlayerCreate.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { withTranslation } from 'react-i18next'
+import PlayerApi from '../../services/PlayerApi';
+import toastr from 'toastr'
+import 'toastr/build/toastr.min.css'
 
 function PlayerCreate({t}) {
 
+  // STATE
   const[isRead, setİsRead] = useState(false);
   const[isActive, setİsActive] = useState(false);
   const[fullName, setFullName] = useState("");
   const[shoeSize, setShoeSize] = useState("");
   const[team, setTeam] = useState("");
-  const[playerDto, setPlayerDto] = useState([]);
+
 
   const changeFullName = (event) => {
     setFullName(event.target.value);
@@ -19,7 +23,6 @@ function PlayerCreate({t}) {
   const changeShoeSize = (event) => {
     setShoeSize(event.target.value);
   }
-
 
   const isReadChecked = (event) => {
     setİsRead(event.target.checked);
@@ -39,12 +42,39 @@ function PlayerCreate({t}) {
     setİsRead(true)
   }
 
+  const addPlayer = async(event) => {
+
+    // Browser sen dur birşey yapma !
+    event.preventDefault();
+    
+    const playerDto = {fullName, team, shoeSize}
+    console.log(playerDto);
+
+    toastr.options = {
+      closeButton: true,
+      progressBar: true,
+    }
+
+    PlayerApi.Create(playerDto)
+    .then((response) => {
+      console.log(response);
+      if(response.status == 201){
+        toastr.success('Player is added.', `${playerDto.fullName}`, {timeOut: 2000})
+      }
+    })
+    .catch((err) => {
+      let validattion = document.getElementById("validation");
+      validattion.innerText = `Error Message: ${err}`;
+    })
+  }
+
   return (
     <>
       <div className="container my-4">
         <div className="row">
           <div className="col-6 offset-3">
             <h1 className='text-center text-primary mb-4'>{t(("addplayer"))}</h1>
+            <div className='fs-6 text-danger px-3 mb-3' id='validation'></div>
             <div className="form-floating mb-3">
               <input
                 onChange={changeFullName}
@@ -95,7 +125,7 @@ function PlayerCreate({t}) {
             </div>
             <div className='mt-3 text-end'>
               <button onClick={reset} className='btn btn-danger me-2'>{t("reset")}</button>
-              <button disabled={!isRead} className='btn btn-primary'>{t("addplayer")}</button>
+              <button onClick={addPlayer} disabled={!isRead} className='btn btn-primary'>{t("addplayer")}</button>
             </div>
           </div>
         </div>
