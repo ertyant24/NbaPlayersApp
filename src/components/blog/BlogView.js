594637daba0a34b6ff997d873bd89537b6b2c182
ertyant24 <erikciertugrul@gmail.com>
2023-09-25 16:58:51 +0300

    revize

--- src/components/blog/BlogView.js
+++ src/components/blog/BlogView.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function BlogView() {
 
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(`viewId: ${id}`);
 
   const [blogDto, setBlogDto] = useState([]);
 
   useEffect(() => {
     axios.get(`https://64e5fa9609e64530d17f608f.mockapi.io/api/v1/blog/react_project/${id}`)
-    .then((response) => {
-      console.log(response);
-      setBlogDto(response.data);
-    })
-    .catch((err) => {
-      console.log(`Error Message: ${err}`);
-    })
-  },[])
+      .then((response) => {
+        console.log(response);
+        setBlogDto(response.data);
+      })
+      .catch((err) => {
+        console.log(`Error Message: ${err}`);
+      })
+  }, [])
 
   console.log(blogDto)
 
@@ -27,13 +27,19 @@ function BlogView() {
       <div className="container my-5">
         <div className="row">
           <div className="col-6 offset-3 d-flex justify-content-center">
-            <div className="card" style={{width: "400px"}}>
+            <div className="card" style={{ width: "400px" }}>
               <img className="card-img-top" src={blogDto.avatar} alt={blogDto.header} />
               <div className="card-body">
-                <h4 className="card-title">{blogDto.header}</h4>
-                <p className="card-text">{blogDto.content}</p>
+                <div className='mb-3'>
+                  {blogDto.createdAt ? blogDto.createdAt.substring(0, 13) : ""}
+                </div>
+                <h4 className="card-title text-center">{blogDto.header ? blogDto.header.toUpperCase() : ""}</h4>
+                <p className="card-text text-center">{blogDto.content}</p>
               </div>
             </div>
+          </div>
+          <div className='col-6 offset-3 text-center mt-4 '>
+            <Link to="/blog/list" className='btn btn-primary px-5'><i className="fa-solid fa-list-ul me-2"></i>Back to List</Link>
           </div>
         </div>
       </div>
